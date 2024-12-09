f6fc28c35d355a4176012a884b7fe485abb64aee
Omar Khaled <34315215+Omarkh98@users.noreply.github.com>
2024-12-09 16:39:00 +0100

    Update vanilla_RAG.py

--- src/server/vanilla_RAG.py
+++ src/server/vanilla_RAG.py
@@ -13,24 +13,30 @@ from sentence_transformers import SentenceTransformer
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from helpers.utils import load_vector_db
 
-VECTORSTORE_PATH = "vector_index.faiss"
+VECTORSTORE_PATH = "src/server/vector_index_fau.faiss"
 METADATA_PATH = "metadata.json" # Mock chunked data [text, metadata[source]]
-METADATA_PATH_FAU = "quality_html-pdf.jsonl" # FAU chunked data [text, url, file_path, chunk_no, dl_date, chunk_date, quality_score]
+METADATA_PATH_FAU = "src/server/quality_html-pdf.jsonl" # FAU chunked data [text, url, file_path, chunk_no, dl_date, chunk_date, quality_score]
+
+# Loading the models once
+RETRIEVER_MODEL = "all-mpnet-base-v2"
+GENERATOR_MODEL = "google/flan-t5-base"
+
+retriever = SentenceTransformer("sentence-transformers/all-mpnet-base-v2") # Hugging Face model
+tokenizer = AutoTokenizer.from_pretrained(GENERATOR_MODEL)
+model = AutoModelForSeq2SeqLM.from_pretrained(GENERATOR_MODEL)
 
 def __init__(self):
     pass
 
 def retrieval(query, index, metadata, top_k = 5):
     try:
-        retriever = SentenceTransformer("sentence-transformers/all-mpnet-base-v2") # Hugging Face model
-        # retriever = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
         query_embedding = retriever.encode([query])
 
         _, indices = index.search(Np.array(query_embedding, dtype = "float32"), top_k)
 
         retrieved_docs = [metadata[i]["text"] for i in indices[0]]
 
-        print("Retrieved Documents:", retrieved_docs)
+        # print("Retrieved Documents:", retrieved_docs)
 
         logging.info("Documents Retrieved Successfully!")
         return retrieved_docs
@@ -43,12 +49,12 @@ def generation(query, retrieved_docs, tokenizer, model):
         logging.disable(logging.CRITICAL)
         logging.info("Creating Prompt...")
         context = " ".join(retrieved_docs)
-        prompt = f"Given the following context, answer the question.\n\nContext: {context}"
+        prompt = f"Please answer this question: {query}, given the following documents: {retrieved_docs}"
 
-        print("Generated Prompt:", prompt)
+        # print("Generated Prompt:", prompt)
 
         inputs = tokenizer(prompt, return_tensors = "pt", truncation = True, max_length = 256)
-        outputs = model.generate(inputs["input_ids"], max_length = 50, num_beams = 3)
+        outputs = model.generate(inputs["input_ids"], max_length = 100, num_beams = 3)
 
         logging.disable(logging.NOTSET)
         logging.info("Output Generated Successfully!")
@@ -62,10 +68,7 @@ if __name__ == "__main__":
     RETRIEVER_MODEL = "all-mpnet-base-v2"
     GENERATOR_MODEL = "google/flan-t5-base"
 
-    tokenizer = AutoTokenizer.from_pretrained(GENERATOR_MODEL)
-    model = AutoModelForSeq2SeqLM.from_pretrained(GENERATOR_MODEL)
-
-    index, metadata = load_vector_db(index_file = VECTORSTORE_PATH, metadata_file = METADATA_PATH)
+    index, metadata = load_vector_db(index_file = VECTORSTORE_PATH, metadata_file = METADATA_PATH_FAU)
 
     Query = "What are the prerequisites for the Data Science course?" # Example Query
 
